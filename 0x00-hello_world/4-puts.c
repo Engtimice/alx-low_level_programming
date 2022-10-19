4bd9c7b362e72949e11c914a3791a7a7201a4635
engtimice <timothyngule@gmail.com>
2022-10-19 09:42:23 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 /**
- * main -Print a line using PUTS
+ * Main - pgogram to dislay message
  *
- * Return: 0
-*/
-int main(void);
-{
-	puts('\"Programming is like building a multilingual puzzle');
+ * Return 0
+ */
+
+int main()
+{  
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
