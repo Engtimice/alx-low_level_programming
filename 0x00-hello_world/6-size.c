8d3d60b5b60d1d623036e908af41f1e9215d4eb5
engtimice <timothyngule@gmail.com>
2022-10-24 11:18:22 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - "program to print size of"
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("(sizeof char), %d\n");
+	printf("(sizeof int), %d\n");
+	printf("sizeof long int, %d\n");
+	printf("sizeof long long int, %d\n");
+	printf("sizeof float, %d\n");
+
+
+	return (0);
+
+}
+
